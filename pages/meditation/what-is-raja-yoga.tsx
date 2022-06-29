5c612313fe157820c6c51e08614704e301cd4149
Lxkas <alohasunshineday@yahoo.com>
2022-06-29 18:41:58 +0700

    what-is page

--- pages/meditation/what-is-raja-yoga.tsx
+++ pages/meditation/what-is-raja-yoga.tsx
@@ -1,5 +1,228 @@
-import React from "react";
-
 export default function WhatIsRajaYoga() {
-	return <div>WhatIsRajaYoga</div>;
+	return (
+		<div className="relative overflow-hidden bg-white py-16">
+			<div className="hidden lg:absolute lg:inset-y-0 lg:block lg:h-full lg:w-full">
+				<div
+					className="relative mx-auto h-full max-w-prose text-lg"
+					aria-hidden="true"
+				>
+					<svg
+						className="absolute top-12 left-full translate-x-32 transform"
+						width="404"
+						height="384"
+						fill="none"
+						viewBox="0 0 404 384"
+					>
+						<defs>
+							<pattern
+								id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+								x="0"
+								y="0"
+								width="20"
+								height="20"
+								patternUnits="userSpaceOnUse"
+							>
+								<rect
+									x="0"
+									y="0"
+									width="4"
+									height="4"
+									className="text-gray-200"
+									fill="currentColor"
+								/>
+							</pattern>
+						</defs>
+						<rect
+							width="404"
+							height="384"
+							fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"
+						/>
+					</svg>
+					<svg
+						className="absolute top-1/2 right-full -translate-y-1/2 -translate-x-32 transform"
+						width="404"
+						height="384"
+						fill="none"
+						viewBox="0 0 404 384"
+					>
+						<defs>
+							<pattern
+								id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+								x="0"
+								y="0"
+								width="20"
+								height="20"
+								patternUnits="userSpaceOnUse"
+							>
+								<rect
+									x="0"
+									y="0"
+									width="4"
+									height="4"
+									className="text-gray-200"
+									fill="currentColor"
+								/>
+							</pattern>
+						</defs>
+						<rect
+							width="404"
+							height="384"
+							fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+						/>
+					</svg>
+					<svg
+						className="absolute bottom-12 left-full translate-x-32 transform"
+						width="404"
+						height="384"
+						fill="none"
+						viewBox="0 0 404 384"
+					>
+						<defs>
+							<pattern
+								id="d3eb07ae-5182-43e6-857d-35c643af9034"
+								x="0"
+								y="0"
+								width="20"
+								height="20"
+								patternUnits="userSpaceOnUse"
+							>
+								<rect
+									x="0"
+									y="0"
+									width="4"
+									height="4"
+									className="text-gray-200"
+									fill="currentColor"
+								/>
+							</pattern>
+						</defs>
+						<rect
+							width="404"
+							height="384"
+							fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"
+						/>
+					</svg>
+				</div>
+			</div>
+			<div className="relative px-4 sm:px-6 lg:px-8">
+				<div className="mx-auto max-w-prose text-lg">
+					<h1>
+						<span className="block text-center text-base font-semibold uppercase tracking-wide text-indigo-600">
+							Meditation
+						</span>
+						<span className="mt-2 block text-center text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-4xl">
+							สมาธิแบบราชาโยคะ
+						</span>
+					</h1>
+					<p className="mt-8 text-xl leading-8 text-gray-500">
+						การทำสมาธิเป็นการเดินทางเข้าสู่ภายใน
+						เป็นการเดินทางเพื่อการค้นพบตนเองอีกครั้ง
+						สมาธิคือเวลาแห่งการใคร่ครวญในความเงียบสงบ
+						ออกมาจากความเร่งรีบและความสับสนวุ่นวายของการใช้ชีวิตประจําวัน
+						การจัดหาเวลานอกเช่นนี้
+						จะช่วยให้เราสามารถกลับคืนสู่ตัวตนภายในที่เงียสงบได้อีกครั้ง
+						ในโลกสมัยใหม่ การใช้ชีวิตที่รีบเร่งขึ้นเรื่อย ๆ
+						ทำให้เราสูญเสียการเชื่อมโยงกับความสงบและพลังภายในที่แท้จริงของตนเอง
+						เมื่อเราไม่สามารถรู้สึกมั่นคงกับตนเองภายในได้
+						เราจึงมีประสบการณ์ของการถูกผลัก ถูกดัน
+						หรือถูกดึงไปในทิศทางต่าง ๆ มากมาย และเริ่มรู้สึกเครียด
+						รู้สึกติดกับอยู่ภายใน และเมื่อเวลาผ่านไป
+						ความรู้สึกนี้ก็อาจนําไปสู่การเจ็บป่วยและโรคภัยต่างๆได้ทีละน้อย
+						ซึ่งเกิดจากการขาดสมดุลในการดูแลสุขภาพของจิตใจ อารมณ์
+						และร่างกาย
+					</p>
+				</div>
+				<div className="prose prose-lg prose-indigo mx-auto mt-6 text-gray-500">
+					{/* <p>
+						Faucibus commodo massa rhoncus, volutpat.{" "}
+						<strong>Dignissim</strong> sed{" "}
+						<strong>eget risus enim</strong>. Mattis mauris semper
+						sed amet vitae sed turpis id. Id dolor praesent donec
+						est. Odio penatibus risus viverra tellus varius sit
+						neque erat velit. Faucibus commodo massa rhoncus,
+						volutpat. Dignissim sed eget risus enim.{" "}
+						<a href="#">Mattis mauris semper</a> sed amet vitae sed
+						turpis id.
+					</p>
+					<ul role="list">
+						<li>Quis elit egestas venenatis mattis dignissim.</li>
+						<li>
+							Cras cras lobortis vitae vivamus ultricies facilisis
+							tempus.
+						</li>
+						<li>
+							Orci in sit morbi dignissim metus diam arcu pretium.
+						</li>
+					</ul>
+					<p>
+						Quis semper vulputate aliquam venenatis egestas sagittis
+						quisque orci. Donec commodo sit viverra aliquam
+						porttitor ultrices gravida eu. Tincidunt leo, elementum
+						mattis elementum ut nisl, justo, amet, mattis. Nunc
+						purus, diam commodo tincidunt turpis. Amet, duis sed
+						elit interdum dignissim.
+					</p>
+					<h2>From beginner to expert in 30 days</h2>
+					<p>
+						Id orci tellus laoreet id ac. Dolor, aenean leo, ac
+						etiam consequat in. Convallis arcu ipsum urna nibh.
+						Pharetra, euismod vitae interdum mauris enim, consequat
+						vulputate nibh. Maecenas pellentesque id sed tellus
+						mauris, ultrices mauris. Tincidunt enim cursus ridiculus
+						mi. Pellentesque nam sed nullam sed diam turpis ipsum eu
+						a sed convallis diam.
+					</p>
+					<blockquote>
+						<p>
+							Sagittis scelerisque nulla cursus in enim
+							consectetur quam. Dictum urna sed consectetur neque
+							tristique pellentesque. Blandit amet, sed aenean
+							erat arcu morbi.
+						</p>
+					</blockquote>
+					<p>
+						Faucibus commodo massa rhoncus, volutpat. Dignissim sed
+						eget risus enim. Mattis mauris semper sed amet vitae sed
+						turpis id. Id dolor praesent donec est. Odio penatibus
+						risus viverra tellus varius sit neque erat velit.
+					</p> */}
+					<figure>
+						<img
+							className="w-full rounded-lg"
+							src="https://www.brahmakumaris.org/documents/10184/19632/raja-yoga-meditation1.jpg/e07635f5-3cf9-43c4-a69a-fe71a89dfe55?t=1454154559432"
+							alt=""
+							width="1310"
+							height="873"
+						/>
+						{/* <figcaption>
+							Sagittis scelerisque nulla cursus in enim
+							consectetur quam.
+						</figcaption> */}
+					</figure>
+					{/* <h2>Everything you need to get up and running</h2> */}
+					<p>
+						{`สมาธิแบบราชาโยคะเป็นสมาธิที่ทุกคนสามารถปฏิบัติได้
+						เป็นการทำสมาธิที่ไม่มีพิธีกรรม ไม่ใช้มันตรา
+						สามารถปฏิบัติได้ทุกที่ทุกเวลา
+						สมาธิแบบราชาโยคะเป็นสมาธิแบบ 'เปิดตา'
+						ที่สามารถใช้ได้ในทุกสถานการณ์ เรียบง่าย
+						และไม่ยากที่จะฝึกฝน`}
+					</p>
+					<p>
+						การทำสมาธิ
+						คือการอยู่ในสภาวะของการรักษาจิตสำนึกให้ไปอยู่เหนือสำนึกรู้ธรรมดาในชีวิตประจำวันทั่วไป
+						ซึ่งเป็นจุดเริ่มต้นของการเสริมสร้างพลังทางจิตวิญญาณ
+						การอยู่ในสำนึกรู้ทางจิตวิญญาณนี้
+						จะทําให้เรามีพลังในการเลือกที่จะคิดความคิดที่ดีและเป็นบวก
+						แทนความคิดที่เป็นลบและไร้ประโยชน์
+						เราจะเริ่มเลือกได้ที่จะตอบสนองต่อสถานการณ์อย่างที่ควรจะเป็น
+						แทนการตอบโต้โดยอัตโนมัติตามความคุ้นชินหรือการถูกกระตุ้น
+						เราจะเริ่มใช้ชีวิตด้วยความสอดคล้องกลมกลืนมากขึ้น
+						มีความสัมพันธ์ที่ดี มีความสุข และเข้มแข็งยิ่งขึ้น
+						และเปลี่ยนชีวิตของเราให้เป็นไปในทางบวกให้มากที่สุด
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
