6eb5b28d8c82f5bfaf49db6bebf1320cd3a502df
Lxkas <alohasunshineday@yahoo.com>
2022-07-13 20:08:01 +0700

    Added random images on navbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,23 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import NavigationBar2 from "./shared/NavigationBar2";
 import SiteFooter from "./shared/SiteFooter";
 
+const randomImages = [
+	"https://www.brahmakumaris.org/images/headers/Head8banner.jpg",
+	"https://images.unsplash.com/photo-1526276828468-fa777352f5aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+	"https://images.unsplash.com/photo-1495567720989-cebdbdd97913?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+];
+
 export default function Layout({ children }: any) {
+	const [randomImg, setRandomImg] = useState(
+		randomImages.sort(() => 0.5 - Math.random())[0]
+	);
+
+	useEffect(() => {
+		setRandomImg(randomImages.sort(() => 0.5 - Math.random())[0]);
+	}, [children]);
+
 	return (
 		<>
 			<Head>
@@ -16,7 +31,7 @@ export default function Layout({ children }: any) {
 			<div className="mb-2 block h-36 w-auto overflow-hidden lg:h-72">
 				<img
 					className="h-full w-full object-cover"
-					src="https://www.brahmakumaris.org/images/headers/Head8banner.jpg"
+					src={randomImg}
 					alt="Banner"
 				/>
 			</div>
