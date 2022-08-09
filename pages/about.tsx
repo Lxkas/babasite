df3bea1d3dbf516b0c2fae5a45b5966111a2721e
Lxkas <alohasunshineday@yahoo.com>
2022-08-09 11:47:18 +0700

    about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,20 +1,164 @@
-import { classNames } from "@/utils/classNames";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import React from "react";
-
-import translation from "@/public/assets/translations/about_page.json";
-
-export default function About() {
-	const { locale } = useRouter();
-
+export default function AboutUsPage() {
 	return (
-		<div>
-			{translation.translation
-				.filter((p) => p.locale === locale)
-				.map((translation, i) => {
-					return <div key={"trans" + i}>{translation.text}</div>;
-				})}
+		<div className="overflow-hidden bg-white py-16 px-4 sm:px-6 lg:px-8 xl:py-36">
+			<div className="mx-auto max-w-max lg:max-w-7xl">
+				<div className="relative z-10 mb-8 md:mb-2 md:px-6">
+					<div className="max-w-prose text-base lg:max-w-none">
+						<h2 className="font-semibold uppercase leading-6 tracking-wide text-indigo-600">
+							รู้จัก
+						</h2>
+						<p className="mt-2 text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-4xl">
+							บราห์มากุมารี
+						</p>
+					</div>
+				</div>
+				<div className="relative">
+					<svg
+						className="absolute top-0 right-0 -mt-20 -mr-20 hidden md:block"
+						width={404}
+						height={384}
+						fill="none"
+						viewBox="0 0 404 384"
+						aria-hidden="true"
+					>
+						<defs>
+							<pattern
+								id="95e8f2de-6d30-4b7e-8159-f791729db21b"
+								x={0}
+								y={0}
+								width={20}
+								height={20}
+								patternUnits="userSpaceOnUse"
+							>
+								<rect
+									x={0}
+									y={0}
+									width={4}
+									height={4}
+									className="text-gray-200"
+									fill="currentColor"
+								/>
+							</pattern>
+						</defs>
+						<rect
+							width={404}
+							height={384}
+							fill="url(#95e8f2de-6d30-4b7e-8159-f791729db21b)"
+						/>
+					</svg>
+					<svg
+						className="absolute bottom-0 left-0 -mb-20 -ml-20 hidden md:block"
+						width={404}
+						height={384}
+						fill="none"
+						viewBox="0 0 404 384"
+						aria-hidden="true"
+					>
+						<defs>
+							<pattern
+								id="7a00fe67-0343-4a3c-8e81-c145097a3ce0"
+								x={0}
+								y={0}
+								width={20}
+								height={20}
+								patternUnits="userSpaceOnUse"
+							>
+								<rect
+									x={0}
+									y={0}
+									width={4}
+									height={4}
+									className="text-gray-200"
+									fill="currentColor"
+								/>
+							</pattern>
+						</defs>
+						<rect
+							width={404}
+							height={384}
+							fill="url(#7a00fe67-0343-4a3c-8e81-c145097a3ce0)"
+						/>
+					</svg>
+					<div className="relative md:bg-white md:p-6">
+						<div className="lg:grid lg:grid-cols-2 lg:gap-6">
+							<div className="prose prose-lg prose-indigo text-gray-500 lg:max-w-none">
+								<p>
+									บราห์มา กุมารี
+									เป็นองค์กรที่เคลื่อนไหวทางจิตวิญญาณที่มีศูนย์การเรียนรู้อยู่ทั่วโลก
+									เพื่อการเปลี่ยนแปลงส่วนบุคคลและการสร้างโลกที่น่าอยู่
+								</p>
+								<p>
+									บราห์มา กุมารีก่อตั้งขึ้นในประเทศอินเดีย
+									ในปีค.ศ.1937
+									ปัจจุบบันได้ขยายศูนย์การเรียนรู้ไปกว่า 110
+									ประเทศทั่วทุกทวีป
+									และให้คุณประโยชน์อย่างกว้างขวางในหลายภาคส่วน
+									ในฐานะที่เป็นองค์กรเอกชนระดับนานาชาติ
+								</p>
+								อย่างไรก็ตาม ข้อผูกมัดที่แท้จริงของบราห์มา
+								กุมารี คือ
+								การช่วยเหลือให้แต่ละบุคคลเปลี่ยนแปลงมุมมองที่พวกเขามีต่อโลก
+								เปลี่ยนจากมุมมองที่เป็นวัตถุนิยมเป็นมุมมองทางจิตวิญญาณ
+								<p>
+									บราห์มากุมารีสนับสนุนการพัฒนาสำนึกรู้ของความสงบร่วมกันที่ลึกล้ำ
+									และ
+									การพัฒนาเกียรติศักดิ์ศรีและคุณค่าความเป็นมนุษย์ของแต่ละบุคคล
+								</p>
+								<p className="font-bold">ศูนย์ประสานงาน</p>
+								<p>
+									สำนักงานใหญ่ทางจิตวิญญาณของบราห์มากุมารี
+									ตั้งอยู่บนภูเขาอาบู รัฐราชสถาน อินเดีย
+								</p>
+							</div>
+							<div className="prose prose-lg prose-indigo mt-6 text-gray-500 lg:mt-0">
+								<p className="font-bold">ในระดับประเทศ</p>
+								<p>
+									โดยทั่วไปแล้วผู้คนในแต่ละพื้นที่จะเป็นหน่วยงานที่ประสานงานในการจัดกิจกรรมต่างๆ
+									ตามแนวทางเดียวกับหลักการทางจิตวิญญาณของ
+									บราห์มากุมารี
+									และทำงานสอดคล้องกับกฎหมายของแต่ละประเทศ
+									กิจกรรมในระดับนานาชาตินั้นจะเป็นการประสานงานในระดับภูมิภาคจากสำนักงานที่ตั้งอยู่ในกกรุงลอนดอน
+									มอสโคว์ ไนโรบี นิวยอร์ค และซิดนีย์
+								</p>
+								<p className="font-bold">
+									บทบาทของสตรีในฐานะผู้นำทางจิตวิญญาณ
+								</p>
+
+								<p>
+									บราห์มา กุมารี
+									เป็นองค์กรทางจิตวิญญาณที่นำโดยสตรี
+									ที่มีขนาดใหญ่ที่สุดในโลก ก่อตั้งโดยประชาบิดา
+									บราห์มา บาบา
+									ผู้ที่เลือกที่จะให้ผู้หญิงอยู่เบื้องหน้า
+									นับแต่ช่วงเวลาเริ่มต้นของการก่อตั้ง
+									และสิ่งนี้เองได้ทำให้บราห์มากุมารีแตกต่างบนเวทีขององค์กรทางจิตวิญญาณและศาสนาของโลก
+									นับเป็นเวลากว่า 80
+									ปีที่การบริหารงานขององค์กรนี้ได้แสดงถึงความกล้าหาญที่มั่นคง
+									ความสามารถที่จะให้อภัย และ
+									ข้อผูกมัดที่ลึกล้ำต่อความเป็นหนึ่งเดียวกัน
+								</p>
+
+								<p>
+									แม้ว่าสตรีจะอยู่ในตำแหน่งผู้บริหารสูงสุด
+									กระนั้นสตรีในตำแหน่งเหล่านั้นก็ทำการตัดสินใจร่วมกันกับบุรุษเสมอ
+									เป็นแบบอย่างการบริหารของความร่วมมือที่มีส่วนร่วมและเป็นเอกฉันท์
+									บนพื้นฐานของความเคารพ ความเสมอภาค
+									และความถ่อมตน เหตุนี้เอง
+									บราห์มากุมารีจึงกลายเป็นแบบอย่างที่ดีเยี่ยมขององค์กรที่มีอำนาจที่สอดคล้องกลมกลืนและประสบความสำเร็จ
+								</p>
+							</div>
+						</div>
+						{/* <div className="mt-8 inline-flex rounded-md shadow">
+							<a
+								href="#"
+								className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700"
+							>
+								Contact sales
+							</a>
+						</div> */}
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
